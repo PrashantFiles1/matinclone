1f9b4f834c27e53fa86dd048c116ae23b15b3d86
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-02 16:38:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PrashantFiles1/matinclone
-cd /movie search bot
-pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting matinclone..."
+git clone https://github.com/PrashantFiles1/matinclone.git /MovieSearchBot
+cd /MovieSearchBot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
 python3 main.py
